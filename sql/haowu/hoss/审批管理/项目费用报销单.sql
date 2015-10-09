996f4be1c93c91e37fdcb4d47d8eed7d821bd53f
yangliang0818 <yangliang0818@126.com>
2015-10-09 19:06:16 +0800

    提交人：杨亮 备注：一客多房功能开发

--- sql/haowu/hoss/审批管理/项目费用报销单.sql
+++ sql/haowu/hoss/审批管理/项目费用报销单.sql
@@ -48,104 +48,20 @@ WHERE fee.wf_instance_id = instance.id
           ) ;
 ---优化后SQL 运行时间0.1185秒
 SELECT
-  fee.id,
-  fee.title,
-  fee.flow_no              AS flowNo,
-  fee.fee_type_name        AS feeTypeName,
-  fee.sub_fee_type_name    AS subFeeTypeName,
-  instance.start_user_name AS startUserName,
-  instance.start_time      AS startTime
-FROM cm_project_fee fee JOIN wf_instance instance
-    ON fee.wf_instance_id = instance.id
-WHERE instance.status IN (2) AND fee.project_id = 1003743741 AND exists(SELECT feeInfo.id
-                                                                        FROM cm_project_fee_info feeInfo
-                                                                        WHERE fee.id = feeInfo.fee_id)
-      AND NOT exists(SELECT contractFeeInfo.fee_info_id
-                     FROM cm_apply_contract contract,
-                       cm_apply_contract_fee_info contractFeeInfo,
-                       wf_instance wf
-                     WHERE
-                       wf.id = contract.wf_instance_id
-                       AND contractFeeInfo.apply_contract_id = contract.id
-                       AND contract.fee_id = fee.id
-                       AND wf.STATUS IN (1, 2, 5, 6)
-)
-      AND NOT exists(SELECT repayInfo.fee_info_id
-                     FROM cm_project_repay repay,
-                       cm_project_repay_info repayInfo,
-                       wf_instance wf
-                     WHERE repay.id = repayInfo.repay_id
-                           AND repay.wf_instance_id = wf.id
-                           AND repay.fee_id = fee.id
-                           AND wf.STATUS IN (1, 2, 5, 6)
-);
---参考方案1 运行时间0.95秒
-SELECT
-  fee.id,
-  fee.title,
-  fee.flow_no              AS flowNo,
-  fee.fee_type_name        AS feeTypeName,
-  fee.sub_fee_type_name    AS subFeeTypeName,
-  instance.start_user_name AS startUserName,
-  instance.start_time      AS startTime
-FROM cm_project_fee fee JOIN wf_instance instance
-    ON fee.wf_instance_id = instance.id
-  join cm_project_fee_info feeInfo on fee.id = feeInfo.fee_id
-WHERE instance.status IN (2) AND fee.project_id = 1003743741
-      AND NOT exists(SELECT contractFeeInfo.fee_info_id
-                     FROM cm_apply_contract contract,
-                       cm_apply_contract_fee_info contractFeeInfo,
-                       wf_instance wf,
-                       cm_project_fee projectFee
-                     WHERE contract.fee_id = projectFee.id AND
-                           wf.id = contract.wf_instance_id
-                           AND contractFeeInfo.apply_contract_id = contract.id
-                           and contractFeeInfo.fee_info_id=feeInfo.id
-                           AND wf.STATUS IN (1, 2, 5, 6)
-)
-      AND NOT exists(SELECT repayInfo.fee_info_id
-                     FROM cm_project_repay repay,
-                       cm_project_repay_info repayInfo,
-                       wf_instance wf,
-                       cm_project_fee projectFee
-                     WHERE repay.id = repayInfo.repay_id
-                           AND repay.wf_instance_id = wf.id
-                           AND repay.fee_id = projectFee.id
-                           and repayInfo.fee_info_id=feeInfo.id
-                           AND wf.STATUS IN (1, 2, 5, 6)
-) group by fee_id;
----参考方案2 运行时间1.28秒
-SELECT
-  fee.id,
-  fee.title,
-  fee.flow_no              AS flowNo,
-  fee.fee_type_name        AS feeTypeName,
-  fee.sub_fee_type_name    AS subFeeTypeName,
-  instance.start_user_name AS startUserName,
-  instance.start_time      AS startTime
-FROM cm_project_fee fee JOIN wf_instance instance
-    ON fee.wf_instance_id = instance.id join
-    cm_project_fee_info feeInfo on fee.id = feeInfo.fee_id
-WHERE instance.status IN (2) AND fee.project_id = 1003743741
-      AND NOT exists(SELECT contractFeeInfo.fee_info_id
-                     FROM cm_apply_contract contract,
-                       cm_apply_contract_fee_info contractFeeInfo,
-                       wf_instance wf
-                     WHERE
-                       wf.id = contract.wf_instance_id
-                       AND contractFeeInfo.apply_contract_id = contract.id
-                       AND contract.fee_id = fee.id
-                       AND wf.STATUS IN (1, 2, 5, 6)
-)
-      AND NOT exists(SELECT repayInfo.fee_info_id
-                     FROM cm_project_repay repay,
-                       cm_project_repay_info repayInfo,
-                       wf_instance wf
-                     WHERE repay.id = repayInfo.repay_id
-                           AND repay.wf_instance_id = wf.id
-                           AND repay.fee_id = fee.id
-                           AND wf.STATUS IN (1, 2, 5, 6)
-);
+            fee.id,
+            fee.title,
+            fee.flow_no              AS flowNo,
+            fee.fee_type_name        AS feeTypeName,
+            fee.sub_fee_type_name    AS subFeeTypeName,
+            instance.start_user_name AS startUserName,
+            instance.start_time      AS startTime
+            FROM cm_project_fee fee join cm_project_fee_info feeInfo on fee.id = feeInfo.fee_id JOIN wf_instance instance ON fee.wf_instance_id = instance.id
+            WHERE instance.status IN (2) AND fee.project_id = 1003743741
+            AND NOT exists(SELECT contractFeeInfo.fee_info_id FROM cm_apply_contract contract,cm_apply_contract_fee_info contractFeeInfo,wf_instance wf
+                WHERE wf.id = contract.wf_instance_id AND contractFeeInfo.apply_contract_id = contract.id AND contract.fee_id = fee.id and contractFeeInfo.fee_info_id=feeInfo.id AND wf.STATUS IN (1, 2, 5, 6))
+            AND NOT exists(SELECT repayInfo.fee_info_id FROM cm_project_repay repay,cm_project_repay_info repayInfo,wf_instance wf
+                WHERE repay.id = repayInfo.repay_id AND repay.wf_instance_id = wf.id AND repay.fee_id = fee.id and repayInfo.fee_info_id=feeInfo.id AND wf.STATUS IN (1, 2, 5, 6))
+            group by fee.id;
 
 select * from cm_project_fee fee where  fee.id=1003833831 and fee.project_id=1003743741;
 select * from cm_project_fee_info where fee_id=1003833831;#1003834833
