82b485b57d2a6517b199f3b97c46d6abfa00955a
yangliang0818 <yangliang0818@126.com>
2016-01-26 15:17:44 +0800

    提交人：杨亮

---
+++ src/com/demo/TestFixPhone.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+/**
+ * 上海好屋网信息技术有限公司
+ * Copyright (C), 2012-2015
+ * Author:   YangLiang 003631
+ * Date:     2015/11/13
+ * Description:
+ */
+public class TestFixPhone {
+    public static void main(String[] args) {
+        String rex="0\\d{2}-\\d{8}|0\\d{2}-\\d{7}|0\\d{3}-\\d{7}|0\\d{3}-\\d{8}";
+
+        System.out.println();
+    }
+}
