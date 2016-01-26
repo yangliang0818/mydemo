06355068efe31a7540a86de7c997bad37d51b64b
yangliang0818 <yangliang0818@126.com>
2016-01-26 15:17:54 +0800

    提交人：杨亮

--- src/com/demo/TestDemo.java
+++ src/com/demo/TestDemo.java
@@ -11,7 +11,15 @@ public class TestDemo {
         String regex = "^[0-9]";
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher("360");
-        System.out.println(matcher.find());
-        System.out.println(new TestMethodInnerClass.TestMyClass().name);
+        /*System.out.println(matcher.find());
+        System.out.println(new TestMethodInnerClass.TestMyClass().name);*/
+        double x = 94;
+        double y = 84;
+        double z = 80;
+        System.out.println(x * 0.5 + y * 0.35 + z * 0.15);
+        /*double x1=100;
+        double y1=83.4;
+        double z1=83;
+        System.out.println(x1 * 0.5 + y1 * 0.35 + z1 * 0.15);*/
     }
 }
