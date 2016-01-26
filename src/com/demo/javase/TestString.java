a014adc497795642df980d2078cbea4be3700ec2
yangliang0818 <yangliang0818@126.com>
2016-01-26 15:22:36 +0800

    提交人：杨亮

---
+++ src/com/demo/javase/TestString.java
@@ -0,0 +1,16 @@
+package com.demo.javase;
+
+import java.util.Arrays;
+
+/**
+ * 上海好屋网信息技术有限公司
+ * Copyright (C), 2012-2015
+ * Author:   YangLiang 003631
+ * Date:     2015/11/24
+ * Description:
+ */
+public class TestString {
+    public static void main(String[] args) {
+        System.out.println(Arrays.asList(" ".split(",")));
+    }
+}
